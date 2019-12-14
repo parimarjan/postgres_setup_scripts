53cfcb243451ed2cbb7ec9fb6f05c50ae2eb3a91
parimarjan <parimarjannegi@gmail.com>
2019-12-14 04:15:55 -0500

    reducing runtime dependencies

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,49 +1,42 @@
 #!/usr/bin/env bash
+
 sudo apt-get update
-sudo apt install --assume-yes vim default-jre maven docker.io python3-pip fish
-sudo apt install --assume-yes cmake silversearcher-ag
+sudo apt install --assume-yes vim python3-pip cmake
 echo "source ~/.bashrc_exports" >> ~/.bashrc
 
 # setup query-optimizer repo
-cd ~/
-git clone https://github.com/parimarjan/query-optimizer.git
-echo "export QUERY_OPT_PATH=/home/ubuntu/query-optimizer" >> ~/.bashrc_exports
-
-git clone https://github.com/park-project/park.git ~/park
-cd ~/park
-git checkout cardinalities
-
-# park_agents isn't needed for now
 #cd ~/
-#git clone https://github.com/hongzimao/park_agents.git
-#cd ~/park_agents
-#git checkout qopt-agent
-#pip3 install -r requirements.txt
+#git clone https://github.com/parimarjan/query-optimizer.git
+#echo "export QUERY_OPT_PATH=/home/ubuntu/query-optimizer" >> ~/.bashrc_exports
 
-cd ~/
-git clone https://github.com/hongzimao/gcn_pytorch.git ~/gcn_pytorch
-echo "export PYTHONPATH=\$PYTHONPATH:/home/ubuntu/park/:/home/ubuntu/gcn_pytorch" >> ~/.bashrc_exports
+#git clone https://github.com/park-project/park.git ~/park
+#cd ~/park
+#git checkout cardinalities
+
+#cd ~/
+#git clone https://github.com/hongzimao/gcn_pytorch.git ~/gcn_pytorch
+#echo "export PYTHONPATH=\$PYTHONPATH:/home/ubuntu/park/:/home/ubuntu/gcn_pytorch" >> ~/.bashrc_exports
 
 cd ~/
 git clone https://github.com/parimarjan/learned-cardinalities.git
 cd learned-cardinalities
 pip3 install -r requirements.txt
 
-echo "export PGM_DIR=/home/ubuntu/learned-cardinalities/pgm/cpp/" >> ~/.bashrc_exports
+#echo "export PGM_DIR=/home/ubuntu/learned-cardinalities/pgm/cpp/" >> ~/.bashrc_exports
 
-cd ~/
-git clone https://github.com/parimarjan/sql_representation.git
-echo "export PYTHONPATH=\$PYTHONPATH:/home/ubuntu/park/:/home/ubuntu/sql_representation/" >> ~/.bashrc_exports
+#cd ~/
+#git clone https://github.com/parimarjan/sql_representation.git
+#echo "export PYTHONPATH=\$PYTHONPATH:/home/ubuntu/park/:/home/ubuntu/sql_representation/" >> ~/.bashrc_exports
 
-cd ~/postgres_setup_scripts
-tar -xvf ./queries.tar.gz
-mv ./queries ~/learned-cardinalities/
+#cd ~/postgres_setup_scripts
+#tar -xvf ./queries.tar.gz
+#mv ./queries ~/learned-cardinalities/
 
 # TMP: just stuff for personal use
-cd ~/
-git clone https://github.com/parimarjan/configs.git
-cd configs
-sh setup.sh
-cd ..
-echo "export PYTHONPATH=\$PYTHONPATH:/home/ubuntu/learned-cardinalities/pgm/python" >> ~/.bashrc_exports
-echo "export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:/home/ubuntu/learned-cardinalities/pgm/cpp" >> ~/.bashrc_exports
+#cd ~/
+#git clone https://github.com/parimarjan/configs.git
+#cd configs
+#sh setup.sh
+#cd ..
+#echo "export PYTHONPATH=\$PYTHONPATH:/home/ubuntu/learned-cardinalities/pgm/python" >> ~/.bashrc_exports
+#echo "export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:/home/ubuntu/learned-cardinalities/pgm/cpp" >> ~/.bashrc_exports
