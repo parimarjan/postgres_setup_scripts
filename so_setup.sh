16cdfa3c27cfcdbfc95e4ecff666993ed97120aa
parimarjan <parimarjannegi@gmail.com>
2020-09-26 01:16:54 -0400

    added stackoverflow setup

--- so_setup.sh
+++ so_setup.sh
@@ -1,5 +1,5 @@
 createdb so
 #wget -O pg_imdb.tar cs.brandeis.edu/~rcmarcus/pg_imdb.tar
-
-tar xfv payload/so_pg12.tar -C ./
+wget -O so_pg12 https://www.dropbox.com/s/q66sw2k6fnugsse/so_pg12?dl=1
+#tar xfv payload/so_pg12.tar -C ./
 pg_restore -v -d so ./so_pg12
