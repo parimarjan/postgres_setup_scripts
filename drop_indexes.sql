107de2481285b3cfd3d0953216004f7ed127177a
Ubuntu <ubuntu@ip-172-31-2-15.us-east-2.compute.internal>
2020-10-03 22:52:50 +0000

    added drop index script

---
+++ drop_indexes.sql
@@ -0,0 +1,11 @@
+DROP INDEX answer_site_id_question_id_idx;
+DROP INDEX answer_creation_date_idx;
+DROP INDEX comment_site_id_post_id_idx;
+DROP INDEX comment_site_id_post_id_idx1;
+DROP INDEX comment_site_id_user_id_idx;
+DROP INDEX post_link_site_id_post_id_to_idx;
+DROP INDEX question_site_id_id_idx;
+DROP INDEX question_creation_date_idx;
+DROP INDEX so_user_creation_date_idx;
+DROP INDEX so_user_last_access_date_idx;
+DROP INDEX tag_question_site_id_tag_id_question_id_idx;
