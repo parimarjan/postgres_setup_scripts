d8cbbd37f42445820e0c95de87137191a706f220
parimarjan <parimarjannegi@gmail.com>
2021-03-14 22:14:02 -0400

    stuff minors

--- init.sh
+++ init.sh
@@ -3,10 +3,12 @@ echo "disk setup done!"
 ./bootstrap.sh
 echo "bootstrap done"
 sleep 3
-bash mysql/mysql_setup.sh
+cd mysql
+bash mysql_setup.sh
 echo "mysql setup done"
 sleep 1
 source ~/.bashrc
-bash mysql/download_csv_files.sh
+
+bash download_csv_files.sh
 echo "imdb setup done!"
 
