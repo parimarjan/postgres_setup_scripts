c27dcd78e502dadea8df72238c42c91b7aee0c7f
Ubuntu <ubuntu@ip-172-31-47-66.us-east-2.compute.internal>
2021-04-25 09:05:04 +0000

    minor fix

--- docker_setup.sh
+++ docker_setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo apt-get install docker.io
+sudo apt-get install --assume-yes docker.io
 
 sudo mv /var/lib/docker /pgfs/
 sudo ln -s /pgfs/docker/ /var/lib/docker
@@ -8,7 +8,7 @@ sudo systemctl restart docker
 
 cd docker
 export LCARD_USER=ubuntu
-export LCARD_PORT=5433
+export LCARD_PORT=5432
 
 sudo docker build --build-arg LCARD_USER=${LCARD_USER} -t pg12 .  
 sudo docker run --memory "512mb" --name card-db -p ${LCARD_PORT}:5432 -d pg12
