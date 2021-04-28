a50bec160637269d5338d01447abde5e573a35e8
Ubuntu <ubuntu@ip-172-31-37-95.us-east-2.compute.internal>
2021-04-28 07:41:32 +0000

    changing instance type

--- disk_setup.sh
+++ disk_setup.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-diskname=$(lsblk | grep 69 | cut -f 1 -d " ")
+#diskname=$(lsblk | grep 69 | cut -f 1 -d " ")
+diskname=$(lsblk | grep 55G | cut -f 1 -d " ")
+#diskname=nvme1n1
+echo $diskname
 sudo rm -rf /pgfs
 sudo mkdir -m 777 /pgfs
 sudo mkfs -F -t ext4 /dev/$diskname
