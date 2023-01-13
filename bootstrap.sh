0c64dacdaeb4f14655ae0d384bee70cf29c039dd
parimarjan <parimarjannegi@gmail.com>
2023-01-13 18:37:36 -0500

    LC fix

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,12 @@ sudo apt-get update
 sudo apt install --assume-yes vim python3-pip cmake
 echo "source ~/.bashrc_exports" >> ~/.bashrc
 
+#cd ~/
+#git clone https://github.com/parimarjan/learned-cardinalities.git
+#cd learned-cardinalities
+#pip3 install -r requirements_rt.txt
+
 cd ~/
-git clone https://github.com/parimarjan/learned-cardinalities.git
-cd learned-cardinalities
-pip3 install -r requirements_rt.txt
+git clone https://github.com/parimarjan/LatencyCollector.git
+cd LatencyCollector
+pip3 install -r requirements.txt
