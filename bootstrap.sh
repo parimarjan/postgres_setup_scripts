37def8ef0921c4683d608ae8d8fb3ed3af6b6cf1
parimarjan <parimarjannegi@gmail.com>
2023-01-14 18:58:11 -0500

    sar

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,3 +15,6 @@ cd ~/
 git clone https://github.com/parimarjan/LatencyCollector.git
 cd LatencyCollector
 pip3 install -r requirements.txt
+
+mkdir -p payload/results
+sar -A -o payload/results/sar.log 10 2400 >/dev/null 2>&1 &
