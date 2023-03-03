0c3aaf75eb8f26888f85379824d720a1a77f3186
parimarjan <parimarjannegi@gmail.com>
2023-03-03 01:38:07 -0500

    minor

--- init.sh
+++ init.sh
@@ -17,10 +17,10 @@ sleep 3
 ./pg_hint_setup.sh
 
 #sleep 3
-./imdb_setup.sh
+#./imdb_setup.sh
 #bash ergast_setup.sh
 #bash stats_ceb_setup.sh
 #bash tpch_setup.sh
-#bash stack_setup.sh
+bash stack_setup.sh
 
 cp ~/postgres_setup_scripts/postgresql.conf $PG_DATA_DIR/
