84c168d6df373ef342392d9332937e829975d220
parimarjan <parimarjannegi@gmail.com>
2023-03-05 13:20:10 -0500

    tpcds

--- init.sh
+++ init.sh
@@ -20,9 +20,10 @@ sleep 3
 #./imdb_setup.sh
 #bash ergast_setup.sh
 #bash stats_ceb_setup.sh
-bash tpch_setup.sh
+#bash tpch_setup.sh
 #bash stack_setup.sh
 #bash tpch_setup2.sh
+bash tpcds_setup.sh
 
 cp ~/postgres_setup_scripts/postgresql.conf $PG_DATA_DIR/
 
