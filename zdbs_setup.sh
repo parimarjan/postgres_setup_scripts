7d477af4ca09c9079fefa34b3c1a0ecafb6b45f1
parimarjan <parimarjannegi@gmail.com>
2023-05-03 19:42:49 -0400

    init

--- zdbs_setup.sh
+++ zdbs_setup.sh
@@ -4,9 +4,9 @@ wget -O walmart.tar https://www.dropbox.com/s/w9fju2mqov5jd8o/walmart.tar?dl=1
 createdb walmart
 pg_restore -v -d walmart ./walmart.tar
 
-wget -O movielens.tar https://www.dropbox.com/s/awzmrqf3cg8dla8/movielens.tar?dl=1
-createdb movielens
-pg_restore -v -d movielens ./movielens.tar
+#wget -O movielens.tar https://www.dropbox.com/s/awzmrqf3cg8dla8/movielens.tar?dl=1
+#createdb movielens
+#pg_restore -v -d movielens ./movielens.tar
 
 wget -O financial.tar https://www.dropbox.com/s/07n8rnax1v9cymt/financial.tar?dl=1
 createdb financial
@@ -16,13 +16,13 @@ wget -O credit.tar https://www.dropbox.com/s/4gny208qbbdbgwh/credit.tar?dl=1
 createdb credit
 pg_restore -v -d credit ./credit.tar
 
-wget -O baseball.tar https://www.dropbox.com/s/38if73psx03jidh/baseball.tar?dl=1
-createdb baseball
-pg_restore -v -d baseball ./baseball.tar
+#wget -O baseball.tar https://www.dropbox.com/s/38if73psx03jidh/baseball.tar?dl=1
+#createdb baseball
+#pg_restore -v -d baseball ./baseball.tar
 
-wget -O accidents.tar https://www.dropbox.com/s/nd9ewgssczlo53j/accidents.tar?dl=1
-createdb accidents
-pg_restore -v -d accidents ./accidents.tar
+#wget -O accidents.tar https://www.dropbox.com/s/nd9ewgssczlo53j/accidents.tar?dl=1
+#createdb accidents
+#pg_restore -v -d accidents ./accidents.tar
 
 #wget -O zdbs.tar.gz https://www.dropbox.com/s/pb413g1523882t7/zdbs.tar.gz?dl=1
 #tar -xvf zdbs.tar.gz
