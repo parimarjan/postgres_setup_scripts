2dc58771958676b9849cb67effe047e2c03a31f9
Ubuntu <ubuntu@ip-172-31-39-219.us-east-2.compute.internal>
2023-09-08 18:43:52 +0000

    minor

--- duckdb/imdb.sh
+++ duckdb/imdb.sh
@@ -1,4 +1,8 @@
 
+echo "starting wget."
 wget -O imdb_csvs.tar.gz https://www.dropbox.com/scl/fi/mqhcuirgjdtwjxy4mh1ta/imdb_csvs.tar.gz?rlkey=or6n4phmcgbxntagj7uf3nbhp&dl=1
-tar -xvf imdb_csvs.tar.gz
-./duckdb/create_duckdb.sh imdb_csvs imdb.duckdb
+echo "Finished wget."
+sleep 150
+
+tar -xvf ./imdb_csvs.tar.gz
+./duckdb/create_duckdb.sh ./imdb_csvs/ imdb.duckdb
