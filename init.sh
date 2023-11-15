cf3b42906ac47b7ba595cb0d21be6dc89e90dfa1
parimarjan <parimarjannegi@gmail.com>
2023-11-15 17:41:05 -0500

    stack

--- init.sh
+++ init.sh
@@ -23,8 +23,8 @@ sleep 3
 ./imdb_setup.sh
 bash ergast_setup.sh
 #bash stats_ceb_setup.sh
-bash tpch_setup.sh
-#bash stack_setup.sh
+#bash tpch_setup.sh
+bash stack_setup.sh
 #bash tpch_setup2.sh
 #bash tpcds_setup.sh
 #bash zdbs_setup.sh
