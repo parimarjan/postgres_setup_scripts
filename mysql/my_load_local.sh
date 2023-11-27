a22f4223772422f8a9116dacdf06a485b7b92652
parimarjan <parimarjannegi@gmail.com>
2023-11-27 06:00:44 +0000

    minor

--- mysql/my_load_local.sh
+++ mysql/my_load_local.sh
@@ -1,1 +1,4 @@
-mysql -u root -D imdb -p --local-infile=1 < test.sql
+
+mysql -u root -D imdb -p --local-infile=1 < create_indexes.sql
+#mysql -u root -D imdb -p --local-infile=1 < schematext2.sql
+#mysql -u root -D imdb -p --local-infile=1 < test.sql
