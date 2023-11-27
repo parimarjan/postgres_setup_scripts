a2b7112c49d61e975a4842e7ec56d4d1702c1232
Ubuntu <ubuntu@ip-172-31-0-21.us-east-2.compute.internal>
2023-11-27 17:55:52 +0000

    fix mysql install

--- mysql/my_load_local.sh
+++ mysql/my_load_local.sh
@@ -3,6 +3,7 @@
 #mysql -u ubuntu -D imdb -ppassword --local-infile=1 < mysql/test.sql
 #mysql -u ubuntu -D imdb -ppassword --local-infile=1 < mysql/create_indexes.sql
 
-mysql -u ubuntu -D imdb --local-infile=1 < mysql/schematext2.sql
-mysql -u ubuntu -D imdb --local-infile=1 < mysql/test.sql
-mysql -u ubuntu -D imdb --local-infile=1 < mysql/create_indexes.sql
+mysql -u ubuntu -e "CREATE DATABASE imdb;"
+mysql -u ubuntu -D imdb --local-infile=1 < schematext2.sql
+mysql -u ubuntu -D imdb --local-infile=1 < test.sql
+mysql -u ubuntu -D imdb --local-infile=1 < create_indexes.sql
