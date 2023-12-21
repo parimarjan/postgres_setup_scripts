7efe88b2e5625e87f10663742a0ff21cf69f6882
Ubuntu <ubuntu@ip-172-31-9-200.us-east-2.compute.internal>
2023-12-21 00:13:38 +0000

    duckdb

---
+++ duckdb/zdbs.sh
@@ -0,0 +1,10 @@
+
+bash duckdb/zdbs_download.sh
+bash duckdb/create_duckdb.sh zdb_csvs/ssb/ ssb.duckdb
+bash duckdb/create_duckdb.sh zdb_csvs/ccs/ ccs.duckdb
+bash duckdb/create_duckdb.sh zdb_csvs/accidents/ accidents.duckdb
+bash duckdb/create_duckdb.sh zdb_csvs/credit/ credit.duckdb
+bash duckdb/create_duckdb.sh zdb_csvs/financial/ financial.duckdb
+bash duckdb/create_duckdb.sh zdb_csvs/seznam/ seznam.duckdb
+bash duckdb/create_duckdb.sh zdb_csvs/basketball/ basketball.duckdb
+
